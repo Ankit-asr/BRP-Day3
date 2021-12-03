2c833478c53e5b9e60a074ed43f857ae0cd0088e
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-12-03 17:22:36 +0530

    [Ankit]Add : Overloading and Overriding Methods

---
+++ BRP-Day3/Polymorphism/MethodOverloading.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class MethodOverloading
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Overloading within same Class");
+            MethodOverloading obj = new MethodOverloading();
+            obj.Test();
+            obj.Test(100);
+            obj.Test("abc");
+            obj.Test(100 , "abc");
+            obj.Test("abc",100);
+            Console.WriteLine("==================================================================================");
+            Console.WriteLine("Overloading and Overriding in different Classes i.e Inheritance");
+            OverloadingAndRiddingUnderChildClass obj1 = new OverloadingAndRiddingUnderChildClass();
+            obj1.Method1();
+            obj1.Method1(100);
+            obj1.Method2();
+        }
+        public void Test()
+        {
+            Console.WriteLine("First Method without any parameters");
+        }
+        public void Test(int number)
+        {
+            Console.WriteLine("Second Method with integer type parameter :"+number);
+        }
+        public void Test(string str)
+        {
+            Console.WriteLine("Third Method with string type parameter :"+str);
+        }
+        public void Test(int number , string str)
+        {
+            Console.WriteLine("Fourth Method with two parameters :"+number+ " "+str);
+        }
+        public void Test(string str , int number)
+        {
+            Console.WriteLine("Fifth Method with two parameters but in different order :"+str+" "+number);
+        }
+    }
+}
---
+++ BRP-Day3/Polymorphism/MethodOverridingLoadingParentClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    class MethodOverridingLoadingParentClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Parent's Class Method1 is called");
+        }
+        public virtual void Method2()
+        {
+            Console.WriteLine("Parent's Class Method2 is called");
+        }
+    }
+    class OverloadingAndRiddingUnderChildClass : MethodOverridingLoadingParentClass
+    {
+        public void Method1(int number)
+        {
+            Console.WriteLine("Child's Class Method1 is called which is Overloading Parent's Method1");
+        }
+        public override void Method2()
+        {
+            Console.WriteLine("Child's Class Method2 is called which is Overrriding Parent's Method2");
+        }
+    }
+}
